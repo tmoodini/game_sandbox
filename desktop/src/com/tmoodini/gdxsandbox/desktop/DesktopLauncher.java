1c277a8214cef81dccc89c0d38e8f0c64d94a927
tmoodini <tom.moody@gmail.com>
2020-06-18 16:58:56 -0600

    First commit

---
+++ desktop/src/com/tmoodini/gdxsandbox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tmoodini.gdxsandbox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tmoodini.gdxsandbox.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
